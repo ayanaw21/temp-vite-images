cefa5c179d531104dccd6ffb0fa64b7340c6c026
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-03-12 04:09:02 -0700

    change the initial value

--- src/context.jsx
+++ src/context.jsx
@@ -22,7 +22,7 @@ const getInitialDarkMode = ()=>{
 
 export const AppProvider = ({ children }) => {
     const [isDarkTheme,setIsDarkTheme] = useState(getInitialDarkMode)
-	const [searchTerm,setSearchTerm] = useState('developer')
+	const [searchTerm,setSearchTerm] = useState('cat')
 
 
     const toggleDarkTheme = ()=>{
